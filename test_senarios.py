7ec4ef29c90851f0a9cbd463f72c4aa00dd188ce
HABOU156 <hanane.boujja.1@ulaval.ca>
2025-01-15 14:22:42 -0500

    test scenarios

---
+++ test_senarios.py
@@ -0,0 +1,17 @@
+SCENARIOS_TEST = {
+    "salutations": [
+        "bonjour",
+        "salut",
+        "hey",
+        "Bonjour tout le monde",
+    ],
+    "au_revoir": [
+        "au revoir",
+        "bye",
+        "à plus tard",
+    ],
+    "phrases_complexes": [
+        "Bonjour, comment allez-vous aujourd'hui?",
+        "Salut, je voudrais poser une question",
+    ]
+}
